c501dda380eef0843f64831eda708d3d2f91a914
'hi.java2' <'hi.java2@gmail.com'>
2020-11-11 16:48:53 +0900

    내려받기

---
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/* 
+ * 작성자: 황일영 
+ * 작성일: 2020년 11월 05일
+ * 설   명: Hello World 출력해보기
+ */
+package com.javaex.helloworld;
+
+public class HelloWorld{
+    
+	public static void main(String[] args){
+		
+		//hello world 출력
+		//여기는 주석이 아님
+	    System.out.println("1l   0oㅇ");
+	}
+}
+
