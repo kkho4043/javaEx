6d5255abcecde782a7fe25120efa7bc2c46e01cf
'kkho4043' <'kkho4043@naver.com'>
2020-11-16 11:00:37 +0900

    pratice

---
+++ src/com/javaex/helloworld/Test01.java
@@ -0,0 +1,19 @@
+package com.javaex.helloworld;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		char s = sc.next().charAt(0);
+		
+		System.out.println(s);
+		sc.close();
+		
+		
+		
+		
+	}
+}
